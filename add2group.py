11a757130ff765e34496e7b9e537ea82a1ac64a2
midoarbi <elmehdinassiri31@gmail.com>
2020-04-10 18:09:17 +0100

    Update add2group.py

--- add2group.py
+++ add2group.py
@@ -112,8 +112,8 @@ for user in users:
 	        else:
 	            sys.exit(re+"[!] Invalid Mode Selected. Please Try Again.")
 	        client(InviteToChannelRequest(target_group_entity,[user_to_add]))
-	        print(gr+"[+] Waiting for 45-95 Seconds...")
-	        time.sleep(random.randrange(45, 95))
+	        print(gr+"[+] Waiting for 60-120 Seconds...")
+	        time.sleep(random.randrange(60, 120))
 	    except PeerFloodError:
 	        print(re+"[!] Getting Flood Error from telegram. \n[!] Script is stopping now. \n[!] Please try again after some time.")
 	    except UserPrivacyRestrictedError:
